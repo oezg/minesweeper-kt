b25bd77626b0e6855cb005aa0619833a72ead903
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-06-05 12:13:46 +0200

    In Progress: idea is to change grid when the mines are set.

--- Grid.kt
+++ Grid.kt
@@ -1,4 +1,30 @@
 package minesweeper
 
-class Grid {
+class Grid(val matrix: List<List<Cell>> = List(SIDE) { List(SIDE) { Cell.Empty() } } ) {
+
+    fun initializeMines(minedCoordinates: Set<Pair<Int, Int>>): Grid {
+        val newMatrix = List(SIDE) { row ->
+            List(SIDE) { col ->
+                val coordinate = row to col
+                if (coordinate in minedCoordinates)
+                    Cell.Mine(marked = getCell(coordinate).isMarked)
+                else
+                    Cell.Empty(countMinesAround(coordinate), marked = getCell(coordinate).isMarked)
+            }
+        }
+        return Grid(newMatrix)
+    }
+
+    fun mark(coordinate: Pair<Int, Int>) {
+        getCell(coordinate).remark()
+    }
+
+    fun getCell(pair: Pair<Int, Int>): Cell = matrix[pair.first][pair.second]
+
+    private fun countMinesAround(my: Pair<Int, Int>): Int = neighbors(my).count { it in minedCoordinates }
+
+    val markedCoordinates
+        get() = matrix.flatMapIndexed { rowIndex, row ->
+            row.mapIndexedNotNull { colIndex, cell ->
+                if (cell.isMarked) rowIndex to colIndex else null } }.toSet()
 }
--- Minesweeper.kt
+++ Minesweeper.kt
@@ -12,36 +12,27 @@ class Minesweeper(private val numberOfMines: Int) {
 
     var state = State.NotExplored
     private var minedCoordinates : Set<Pair<Int,Int>> = emptySet()
-    private var matrix: List<List<Cell>> = List(SIDE) { List(SIDE) { Cell.Empty() } }
-
-    private val markedCoordinates
-        get() = matrix.flatMapIndexed { rowIndex, row ->
-            row.mapIndexedNotNull { colIndex, cell ->
-                if (cell.isMarked) rowIndex to colIndex else null } }.toSet()
-
-    private val unexploredCoordinates
-        get() = matrix.flatMapIndexed { rowIndex, row ->
-            row.mapIndexedNotNull { colIndex, cell ->
-                if (cell.isExplored) null else rowIndex to colIndex } }.toSet()
+    private var grid = Grid()
 
     private fun neighbors(my: Pair<Int, Int>): List<Pair<Int, Int>> =
         OFFSETS
             .filter { my.first + it.first in 0 until SIDE && my.second + it.second in 0 until SIDE }
             .map { my.first + it.first to my.second + it.second }
 
-    private fun countMinesAround(my: Pair<Int, Int>): Int = neighbors(my).count { it in minedCoordinates }
 
-    fun isValid(action: Action): Boolean = !getCell(action.coordinate).isExplored
+
+    fun isValid(action: Action): Boolean = !grid.getCell(action.coordinate).isExplored
 
     fun execute(action: Action) =
         when (action) {
             is Action.Mark -> {
-                getCell(action.coordinate).remark()
-                state = if (markedCoordinates == minedCoordinates) State.Win else state
+                grid.mark(action.coordinate)
+                state = if (grid.markedCoordinates == minedCoordinates) State.Win else state
             }
             is Action.Explore -> {
                 if (state == State.NotExplored) {
-                    initializeMines(action.coordinate)
+                    minedCoordinates = generateDistinctCoordinates(action.coordinate)
+                    grid = grid.initializeMines(minedCoordinates)
                     state = State.NotFinished
                 }
                 explore(action.coordinate)
@@ -53,7 +44,7 @@ class Minesweeper(private val numberOfMines: Int) {
         println()
         println(" │123456789│")
         println("—│—————————│")
-        for ((index, row) in matrix.withIndex()) {
+        for ((index, row) in grid.matrix.withIndex()) {
             print("${index + 1}|")
             for (cell in row) {
                 print(if (state == State.Loss && cell is Cell.Mine) 'X' else cell)
@@ -63,21 +54,10 @@ class Minesweeper(private val numberOfMines: Int) {
         println("—│—————————│")
     }
 
-    private fun initializeMines(firstExploredCoordinate: Pair<Int, Int>) {
-        minedCoordinates = generateDistinctCoordinates(numberOfMines, firstExploredCoordinate)
-        matrix = List(SIDE) { row ->
-            List(SIDE) { col ->
-                val coordinate = row to col
-                if (coordinate in minedCoordinates)
-                    Cell.Mine(marked = getCell(coordinate).isMarked)
-                else
-                    Cell.Empty(countMinesAround(coordinate), marked = getCell(coordinate).isMarked)
-            }
-        }
-    }
+
 
     private fun explore(coordinate: Pair<Int, Int>) {
-        val cell = getCell(coordinate)
+        val cell = grid.getCell(coordinate)
         if (cell is Cell.Mine) {
             state = State.Loss
             return
@@ -92,11 +72,10 @@ class Minesweeper(private val numberOfMines: Int) {
         neighbors(coordinate).forEach { explore(it) }
     }
 
-    private fun getCell(pair: Pair<Int, Int>): Cell = matrix[pair.first][pair.second]
 
-    private fun generateDistinctCoordinates(n: Int, coordinate: Pair<Int, Int>): Set<Pair<Int, Int>> {
+    fun generateDistinctCoordinates(coordinate: Pair<Int, Int>): Set<Pair<Int, Int>> {
         val coordinates = mutableSetOf<Pair<Int, Int>>()
-        while (coordinates.size < n) {
+        while (coordinates.size < numberOfMines) {
             val nextCoordinate = Random.nextInt(SIDE) to Random.nextInt(SIDE)
             if (nextCoordinate == coordinate) continue
             coordinates.add(nextCoordinate)
