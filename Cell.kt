4a020283d21f6f4a9525be4be9eb53225bf45f8d
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-06-05 09:24:14 +0200

    protected variables

--- Cell.kt
+++ Cell.kt
@@ -3,32 +3,30 @@ package minesweeper
 sealed class Cell(marked: Boolean = false, explored: Boolean = false) {
     protected var cellMarked: Boolean = marked
     protected var cellExplored: Boolean = explored
-
+    val isMarked: Boolean get() = cellMarked
+    val isExplored: Boolean get() = cellExplored
     fun remark() {
         cellMarked = !cellMarked
     }
 
-    val isMarked: Boolean get() = this.cellMarked
-    val isExplored: Boolean get() = this.cellMarked
-
     class Empty(val minesAround: Int = 0,
-                isMarked: Boolean = false,
-                isExplored: Boolean = false) : Cell(marked = isMarked, explored = isExplored) {
+                     isMarked: Boolean = false,
+                     isExplored: Boolean = false) : Cell(marked = isMarked, explored = isExplored) {
 
         fun explore() {
             cellExplored = true
-            if (cellMarked) remark()
+            if (isMarked) remark()
         }
 
         override fun toString() = when {
-            cellMarked -> "*"
-            !cellExplored -> "."
+            isMarked -> "*"
+            !isExplored -> "."
             minesAround == 0 -> "/"
             else -> minesAround.toString()
         }
     }
 
     class Mine(isMarked: Boolean = false) : Cell(marked = isMarked) {
-        override fun toString() = if (cellMarked) "*" else "."
+        override fun toString() = if (isMarked) "*" else "."
     }
 }
