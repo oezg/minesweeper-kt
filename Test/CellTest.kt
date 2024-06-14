1edb43227fcf9607744531e72dcf0bb90a45e980
Özgür Yıldırım <ozguryenihayat@gmail.com>
2024-06-14 08:20:15 +0200

    added one test to cell

--- Test/CellTest.kt
+++ Test/CellTest.kt
@@ -1,6 +1,7 @@
 package minesweeper.Test
 
 import minesweeper.Empty
+import minesweeper.Mine
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -9,6 +10,13 @@ class CellTest {
     fun remarkEmpty() {
         val expected = Empty(minesAround = 0, isMarked = true, isExplored = false)
         val sut = Empty(minesAround = 0, isMarked = false, isExplored = false)
+        assertEquals(expected, sut.remark())
+    }
+
+    @Test
+    fun remarkMine() {
+        val expected = Mine(isMarked = true, isExplored = false)
+        val sut = Mine(isMarked = false, isExplored = false)
         assertEquals(expected, sut.remark())
     }
 }
